e49887e101e3e026fc762f2e085365b6fa982c60
flopsi <florian.marty1983@gmail.com>
2025-11-22 00:22:34 +0100

    Add files via upload

---
+++ proteomics_modules/lfqbench__init__.py
@@ -0,0 +1,32 @@
+"""
+LFQbench Analysis Module
+Benchmark proteomics analysis with comprehensive performance metrics.
+"""
+
+from .lfqbench_analysis import (
+    LFQbenchAnalyzer,
+    BenchmarkConfig,
+    get_lfqbench_analyzer
+)
+
+from .lfqbench_visualizations import (
+    LFQbenchVisualizer,
+    get_lfqbench_visualizer
+)
+
+from .lfqbench_module import (
+    LFQbenchModule,
+    run_lfqbench_module
+)
+
+__all__ = [
+    'LFQbenchAnalyzer',
+    'BenchmarkConfig',
+    'get_lfqbench_analyzer',
+    'LFQbenchVisualizer',
+    'get_lfqbench_visualizer',
+    'LFQbenchModule',
+    'run_lfqbench_module'
+]
+
+__version__ = '1.0.0'
