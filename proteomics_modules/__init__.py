2b43908f2f565850bde366fa65b2d2da06647ca1
flopsi <florian.marty1983@gmail.com>
2025-11-21 22:10:22 +0100

    Update __init__.py

--- proteomics_modules/__init__.py
+++ proteomics_modules/__init__.py
@@ -1,5 +1,2 @@
-mkdir -p proteomics_modules/data_upload
-mkdir -p tests
-touch proteomics_modules/__init__.py
-touch proteomics_modules/data_upload/__init__.py
-touch tests/__init__.py
+"""Proteomics Modules PackageModular proteomics data analysis toolkit"""__version__ = "0.1.0"__author__ = "Proteomics Team"from .data_upload import DataUploadModule, run_upload_module
+__all__ = ['DataUploadModule', 'run_upload_module']
