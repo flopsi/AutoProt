6cf234cd973ee25b3c738d0b43e5720031f720d1
flopsi <florian.marty1983@gmail.com>
2025-11-21 22:51:53 +0100

    Update init.py

--- proteomics_modules/data_upload/init.py
+++ proteomics_modules/data_upload/init.py
@@ -1,9 +1,15 @@
-"""Data Upload ModuleHandles file upload, validation, and preprocessing"""from .module import DataUploadModule, run_upload_module
+"""
+Data Upload Module
+Handles file upload, validation, and preprocessing
+"""
+
+from .module import DataUploadModule, run_upload_module
 from .config import get_config, update_config
 from .session_manager import get_session_manager
 from .validators import get_validator
 from .parsers import get_data_parser, get_metadata_parser
 from .column_detector import get_column_detector, get_species_manager
+
 __all__ = [
     'DataUploadModule',
     'run_upload_module',
@@ -14,4 +20,5 @@ __all__ = [
     'get_data_parser',
     'get_metadata_parser',
     'get_column_detector',
-    'get_species_manager']
+    'get_species_manager'
+]
