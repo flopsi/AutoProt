7671ffeab34eafa03e199ee0a0709dfe715bc0a0
flopsi <florian.marty1983@gmail.com>
2025-11-23 21:23:40 +0100

    Delete proteomics_app directory

--- proteomics_app/utils/column_mapper.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-import pandas as pd
-
-def apply_column_mapping(df: pd.DataFrame, mapping: Dict[str, str]) -> pd.DataFrame:
-    return df.rename(columns=mapping)
-
-def validate_even_replicates(mapping: Dict[str, str]) -> bool:
-    return len(mapping) % 2 == 0
