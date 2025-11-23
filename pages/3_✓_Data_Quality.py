d12c23cc537d3be2b4fbe8ad90f80d302cfc8ef8
flopsi <florian.marty1983@gmail.com>
2025-11-24 00:46:27 +0100

    Update 3_✓_Data_Quality.py

--- pages/3_✓_Data_Quality.py
+++ pages/3_✓_Data_Quality.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sl
+import sklearn
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from sklearn.decomposition import PCA
