90131d7bef7ed9bd7e9f146b85499715a82bd1b1
flopsi <florian.marty1983@gmail.com>
2025-11-25 19:26:54 +0100

    Create init.py

---
+++ utils/init.py
@@ -0,0 +1,29 @@
+# Utils package
+from .data_generator import generate_mock_data
+from .analysis import process_data, calculate_stats
+from .stats import (
+    calculate_cv,
+    check_normality,
+    log2_transform,
+    perform_pca,
+    calculate_missing_values,
+    calculate_quartiles,
+    calculate_dynamic_range,
+    perform_t_test,
+    batch_process_proteins
+)
+
+__all__ = [
+    'generate_mock_data',
+    'process_data',
+    'calculate_stats',
+    'calculate_cv',
+    'check_normality',
+    'log2_transform',
+    'perform_pca',
+    'calculate_missing_values',
+    'calculate_quartiles',
+    'calculate_dynamic_range',
+    'perform_t_test',
+    'batch_process_proteins'
+]
