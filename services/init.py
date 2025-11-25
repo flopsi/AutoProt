41aaf5b75e5cdd4e56771f564df86ea774437f0e
flopsi <florian.marty1983@gmail.com>
2025-11-25 20:56:58 +0100

    Update init.py

--- services/init.py
+++ services/init.py
@@ -1,2 +1,12 @@
+"""
+Services package for external integrations
+"""
+
+from .gemini_service import analyze_proteins, chat_with_data
+
+__all__ = [
+    'analyze_proteins',
+    'chat_with_data'
+]
 # Services packagefrom .gemini_service import analyze_proteins, chat_with_data
 __all__ = ['analyze_proteins', 'chat_with_data']
