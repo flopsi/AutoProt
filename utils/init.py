8e6b791001e9daa76b6bdf7cd59e0019ba747841
flopsi <florian.marty1983@gmail.com>
2025-11-25 20:10:03 +0100

    Create init.py

---
+++ utils/init.py
@@ -0,0 +1,3 @@
+# Utils packagefrom .data_generator import generate_mock_data
+from .analysis import process_data, calculate_stats
+__all__ = ['generate_mock_data', 'process_data', 'calculate_stats']
