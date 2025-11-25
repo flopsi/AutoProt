57a13f1f3b0cd2d25c79e9e6e25d9470b4931522
flopsi <florian.marty1983@gmail.com>
2025-11-25 19:27:29 +0100

    Create init.py

---
+++ components/init.py
@@ -0,0 +1,24 @@
+# Components package
+from .plots import create_volcano_plot
+from .tables import render_data_table
+from .stats import render_stats_cards
+from .qc_plots import (
+    render_boxplots,
+    render_cv_analysis,
+    render_pca_plot,
+    render_missing_value_heatmap,
+    render_rank_plots,
+    render_qc_dashboard
+)
+
+__all__ = [
+    'create_volcano_plot',
+    'render_data_table',
+    'render_stats_cards',
+    'render_boxplots',
+    'render_cv_analysis',
+    'render_pca_plot',
+    'render_missing_value_heatmap',
+    'render_rank_plots',
+    'render_qc_dashboard'
+]
