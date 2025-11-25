929c1e231f70fc7fa1785fd3779bc0ceffa955ab
flopsi <florian.marty1983@gmail.com>
2025-11-25 20:10:46 +0100

    Create init.py

---
+++ components/init.py
@@ -0,0 +1,4 @@
+# Components packagefrom .plots import create_volcano_plot
+from .tables import render_data_table
+from .stats import render_stats_cards
+__all__ = ['create_volcano_plot', 'render_data_table', 'render_stats_cards']
