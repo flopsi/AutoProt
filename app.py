c4963e85c21e329b3e7067f5676e1d48654396f1
flopsi <florian.marty1983@gmail.com>
2025-11-26 01:04:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 
 import streamlit as st
-pg = st.navigation([st.Page("page_2.py",title="Protein Import"), st.Page("page_3.py"), st.Page("page_4.py")])
+pg = st.navigation([st.Page("page_2.py",title="Protein Import"), 
+                    st.Page("page_3.py"), 
+                    st.Page("page_4.py")]
+    )
 pg.run()
