8c5b142963f84f4ee6f1a6803586d7304266bede
flopsi <florian.marty1983@gmail.com>
2025-11-26 01:11:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ pg = st.navigation([st.Page("page_2.py",title="Protein Import"),
                     st.Page("page_3.py", title="Peptide Import"), 
                     st.Page("page_4.py", title="Data Quality")]
     )
+st.set_page_config(page_title="autoProt", page_icon ="🧬")
+
 pg.run()
