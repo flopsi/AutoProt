1aa193cae8469029cea7d7e3ef91c499ddf0e13e
flopsi <florian.marty1983@gmail.com>
2025-11-27 22:18:36 +0100

    Update 0_Data_Upload.py

--- pages/0_Data_Upload.py
+++ pages/0_Data_Upload.py
@@ -21,7 +21,7 @@ if not uploaded_prot:
 
 @st.cache_data  # 👈 Add the caching decorator
 def load_prot(file):
-    df = pd.read_csv(file, sep=None, engine="python", header=0)
+    df = pd.read_csv(file, sep=None, engine="python", header=0,index=0)
     return df
 
 df_prot=load_prot(uploaded_prot)
