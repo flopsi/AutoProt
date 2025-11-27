0fa3822885634318afd55fa0cfc5652298737348
flopsi <florian.marty1983@gmail.com>
2025-11-27 22:36:04 +0100

    Update 0_Data_Upload.py

--- pages/0_Data_Upload.py
+++ pages/0_Data_Upload.py
@@ -33,22 +33,4 @@ data_df=pd.DataFrame(
                     }
 )
 
-st.data_editor(
-    data_df,
-    column_config={
-        "widgets": st.column_config.Column(
-            "Streamlit Widgets",
-            help="Streamlit **widget** commands 🎈",
-            width="medium",
-            required=True,
-        )
-    },
-    hide_index=True,
-    num_rows="dynamic",
-)
-st.dataframe(df_prot)
-
-df_prot.head(5)
-
-edited_df_prot = st.data_editor(df_prot)
 
