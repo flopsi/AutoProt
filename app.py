f42872439169d480ed53f23dc3e01cc8b89de727
flopsi <florian.marty1983@gmail.com>
2025-11-29 00:41:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,38 @@ if uploaded_file is not None:
     #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    #df = pd.read_csv(uploaded_file)
+    #st.write(dataframe)
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Animal": ["Lion", "Elephant", "Giraffe", "Monkey", "Zebra"],
+        "Habitat": ["Savanna", "Forest", "Savanna", "Forest", "Savanna"],
+        "Lifespan (years)": [15, 60, 25, 20, 25],
+        "Average weight (kg)": [190, 5000, 800, 10, 350],
+    }
+)
+
+def dataframe_with_selections(df):
+    df_with_selections = df.copy()
+    df_with_selections.insert(0, "Select", False)
+
+    # Get dataframe row-selections from user with st.data_editor
+    edited_df = st.data_editor(
+        df_with_selections,
+        hide_index=True,
+        column_config={"Select": st.column_config.CheckboxColumn(required=True)},
+        disabled=df.columns,
+    )
+
+    # Filter the dataframe using the temporary column, then drop the column
+    selected_rows = edited_df[edited_df.Select]
+    return selected_rows.drop('Select', axis=1)
+
+
+selection = dataframe_with_selections(df)
+st.write("Your selection:")
+st.write(selection)
