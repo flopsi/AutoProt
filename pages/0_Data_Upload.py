2a74628daa81d2b042d0203bb74e8eadf4f6e8bb
flopsi <florian.marty1983@gmail.com>
2025-11-28 03:03:35 +0100

    Update 0_Data_Upload.py

--- pages/0_Data_Upload.py
+++ pages/0_Data_Upload.py
@@ -9,14 +9,16 @@ def load_csv(file):
 uploaded = st.file_uploader("Upload CSV", type=["csv"])
 if uploaded is None:
     st.stop()
-
-df_raw = load_csv(uploaded)
-st.dataframe(df_raw.head())
-
-row0 = df_raw.iloc[0]                 # first row (a Series)
-row0_df = row0.to_frame(name="value") # index = column names, column = value
+row0 = df_raw.iloc[0]
+row0_df = row0.to_frame(name="value")
 row0_df.index.name = "column"
-row0_df.reset_index(inplace=True)     # columns: ["column", "value"]
+row0_df.reset_index(inplace=True)  # columns: ["column", "value"]
+
+# Add helper columns so they exist in edited
+row0_df["use"] = True
+row0_df["is_condition"] = False
+row0_df["is_replicate"] = False
+row0_df["tags"] = [[]] * len(row0_df)  # one empty list per row
 
 edited = st.data_editor(
     row0_df,
@@ -24,26 +26,13 @@ edited = st.data_editor(
         "use": st.column_config.CheckboxColumn("Use?", default=True),
         "is_condition": st.column_config.CheckboxColumn("Is condition?", default=False),
         "is_replicate": st.column_config.CheckboxColumn("Is replicate?", default=False),
-        "tags": st.column_config.ListColumn("Tags"),  # simple tag-like list of strings
+        "tags": st.column_config.ListColumn("Tags"),
     },
     num_rows="fixed",
 )
 
-# Columns user wants to use
-selected_cols = edited.loc[edited["use"], "column"].tolist()
-
-# Condition and replicate columns (flexible naming)
-cond_cols = edited.loc[edited["is_condition"], "column"].tolist()
-rep_cols  = edited.loc[edited["is_replicate"], "column"].tolist()
-
-# Tags per column (list of strings)
-tag_map = dict(zip(edited["column"], edited["tags"]))
+df_raw = load_csv(uploaded)
+st.dataframe(df_raw.head())
 
 
-
-st.write("Condition 1 DF")
-st.dataframe(df_cond1)
-
-st.write("Condition 2 DF")
-st.dataframe(df_cond2)
 
