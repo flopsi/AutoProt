58f5df0a7ba789ecbfce83b90fa0cb6a3fd79f03
flopsi <florian.marty1983@gmail.com>
2025-11-29 00:50:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
 
+
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
 
 
 
