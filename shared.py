9c2a7e32c4c3a22c5112779871ef64fcef604be2
flopsi <florian.marty1983@gmail.com>
2025-11-28 22:55:44 +0100

    Delete shared.py

--- shared.py
+++
@@ -1,9 +0,0 @@
-# shared.py
-import streamlit as st
-
-def restart_button():
-    if st.button("Restart Everything"):
-        for key in list(st.session_state.keys()):
-            del st.session_state[key]
-        st.success("App restarted! Upload new files.")
-        st.rerun()
