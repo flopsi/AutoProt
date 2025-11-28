dd6f78a343ec6fb18d5ad28460ba4429e7bebb57
flopsi <florian.marty1983@gmail.com>
2025-11-29 00:50:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ uploaded_files = st.file_uploader(
     "Upload data", accept_multiple_files=True, type="csv"
 )
 
-
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
-    st.write(df)
+    return df
 
 
 
