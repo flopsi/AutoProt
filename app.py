1dcf3c55dd0762c88ccf6b3f1921f4082d62e917
flopsi <florian.marty1983@gmail.com>
2025-11-28 22:57:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,26 +13,3 @@ st.markdown(
 
 st.info("Use the sidebar navigation on the left to open the upload page.")
 
-def main():
-    st.title("DIA Proteomics Pipeline")
-
-    st.markdown(
-        """
-        Welcome to the DIA Proteomics Pipeline.
-
-        - Start on **Upload Protein and Peptide Data** in the sidebar.
-        - Configure your protein and/or peptide tables there.
-        - Downstream pages (QC, statistics, visualization) will use
-          the processed objects stored in `st.session_state["protein_upload"]`
-          and `st.session_state["peptide_upload"]`.
-        """
-    )
-
-    st.info(
-        "Use the navigation in the sidebar to switch between pages. "
-        "If you only see this text and the sidebar, click on the "
-        "**Upload Protein and Peptide Data** page."
-    )
-
-if __name__ == "__main__":
-    main()
