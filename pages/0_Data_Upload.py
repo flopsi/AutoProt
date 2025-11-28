56b09601a62097867de9966baf1e7c9ef63e881c
flopsi <florian.marty1983@gmail.com>
2025-11-28 02:56:15 +0100

    Update 0_Data_Upload.py

--- pages/0_Data_Upload.py
+++ pages/0_Data_Upload.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 @st.cache_data
 def load_csv(file):
-    return pd.read_csv(file, sep=None, engine="python")  # no fixed index/headers
+    return pd.read_csv(file, sep= , engine="python")  # no fixed index/headers
 
 uploaded = st.file_uploader("Upload CSV", type=["csv"])
 if uploaded is None:
@@ -12,40 +12,34 @@ if uploaded is None:
 
 df_raw = load_csv(uploaded)
 st.dataframe(df_raw.head())
-cols = df_raw.columns.tolist()
 
-df_raw = load_csv(uploaded)
-st.dataframe(df_raw.head())
+row0 = df_raw.iloc[0]                 # first row (a Series)
+row0_df = row0.to_frame(name="value") # index = column names, column = value
+row0_df.index.name = "column"
+row0_df.reset_index(inplace=True)     # columns: ["column", "value"]
 
-
-meta_cols = st.multiselect(
-    "Select metadata columns",
-    options=df_raw.columns.tolist(),
+edited = st.data_editor(
+    row0_df,
+    column_config={
+        "use": st.column_config.CheckboxColumn("Use?", default=True),
+        "is_condition": st.column_config.CheckboxColumn("Is condition?", default=False),
+        "is_replicate": st.column_config.CheckboxColumn("Is replicate?", default=False),
+        "tags": st.column_config.ListColumn("Tags"),  # simple tag-like list of strings
+    },
+    num_rows="fixed",
 )
 
-cond1_col = st.selectbox("Select column for condition 1", df_raw.columns)
-cond2_col = st.selectbox("Select column for condition 2", df_raw.columns)
+# Columns user wants to use
+selected_cols = edited.loc[edited["use"], "column"].tolist()
 
-cond1_value = st.selectbox("Value for condition 1", df_raw[cond1_col].unique())
-cond2_value = st.selectbox("Value for condition 2", df_raw[cond2_col].unique())
+# Condition and replicate columns (flexible naming)
+cond_cols = edited.loc[edited["is_condition"], "column"].tolist()
+rep_cols  = edited.loc[edited["is_replicate"], "column"].tolist()
 
-
-@st.cache_data
-def get_metadata_df(df, meta_cols):
-    return df[meta_cols].copy()
-
-@st.cache_data
-def get_condition_df(df, cond_col, cond_value):
-    return df[df[cond_col] == cond_value].copy()
+# Tags per column (list of strings)
+tag_map = dict(zip(edited["column"], edited["tags"]))
 
 
-if meta_cols:
-    df_meta = get_metadata_df(df_raw, tuple(meta_cols))  # lists must be hashable -> tuple
-    st.write("Metadata DF")
-    st.dataframe(df_meta)
-
-df_cond1 = get_condition_df(df_raw, cond1_col, cond1_value)
-df_cond2 = get_condition_df(df_raw, cond2_col, cond2_value)
 
 st.write("Condition 1 DF")
 st.dataframe(df_cond1)
