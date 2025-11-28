fe01352814d03df87815b4265eb72d92075357dc
flopsi <florian.marty1983@gmail.com>
2025-11-28 03:05:23 +0100

    Update 0_Data_Upload.py

--- pages/0_Data_Upload.py
+++ pages/0_Data_Upload.py
@@ -7,6 +7,8 @@ def load_csv(file):
     return pd.read_csv(file, sep=None , engine="python")  # no fixed index/headers
 
 uploaded = st.file_uploader("Upload CSV", type=["csv"])
+
+
 if uploaded is None:
     st.stop()
 row0 = df_raw.iloc[0]
