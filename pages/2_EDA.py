4a7d4459defbce2ed3ec8f942e74fc0b7f982f26
flopsi <florian.marty1983@gmail.com>
2025-11-30 01:14:07 +0100

    Update 2_EDA.py

--- pages/2_EDA.py
+++ pages/2_EDA.py
@@ -383,7 +383,7 @@ def render_eda(model: MSData | None, index_col: str | None, species_col: str | N
             f"Species detected: {', '.join(species_dist['species'])}. "
             "Showing PCA for: all | dominant | others"
         )
-        col1, col2, col3 = st.columns(3,[1,1,1], vertical_alignment="top",border=True,width="stretch")
+        col1, col2, col3 = st.columns(3,[1,1,1], vertical_alignment="top",border=True)
 
         # Plot 1: All
         with col1:
