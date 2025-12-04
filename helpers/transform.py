8fbf0b516a92e05798cc4f63a9c83c23d84094ba
flopsi <florian.marty1983@gmail.com>
2025-12-04 22:21:02 +0100

    Update transform.py

--- helpers/transform.py
+++ helpers/transform.py
@@ -113,7 +113,7 @@ def log2_transform(
     
     print(f"✅ Log2 transformation complete")
     
-    return df_transformed
+    return df_log2
 
 
 def log10_transform(
@@ -147,7 +147,7 @@ def log10_transform(
     
     print(f"✅ Log10 transformation complete")
     
-    return df_transformed
+    return df_log10
 
 
 def normalize_columns(
