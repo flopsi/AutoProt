070e1b2925d4088d5d58688fa2a842e2b2f33ee8
flopsi <florian.marty1983@gmail.com>
2025-12-05 23:11:41 +0100

    Update evaluation.py

--- helpers/evaluation.py
+++ helpers/evaluation.py
@@ -101,7 +101,7 @@ def create_raw_row_figure(
         fig.add_vrect(
             x0=x0,
             x1=x1,
-            fillcolor="white",
+            fillcolor="#ff0000",
             opacity=0.15,
             line_width=0,
             row=1,
@@ -248,7 +248,7 @@ def create_transformed_row_figure(
         fig.add_vrect(
             x0=x0,
             x1=x1,
-            fillcolor="#whie",
+            fillcolor="#ff0000",
             opacity=0.15,
             line_width=0,
             row=1,
