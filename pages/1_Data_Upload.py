fa4f77a37b63e9d58b32856e53ab50f755d7a149
flopsi <florian.marty1983@gmail.com>
2025-12-08 19:51:42 +0100

    Update 1_Data_Upload.py

--- pages/1_Data_Upload.py
+++ pages/1_Data_Upload.py
@@ -91,6 +91,7 @@ try:
             df_raw = pl.read_csv(uploaded_file,
                                  has_header=True,
                                  null_values = List["#Num!","None"]
+                                )
         else:
             df_raw = pl.read_excel(uploaded_file, sheet_id=0)
         
